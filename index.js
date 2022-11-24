2a8b963916abccc01ddf7d2bc500140dca96e30f
Imtius-Ahammed <ahammedimtius098@gmail.com>
2022-11-24 23:33:32 +0600

    added jwt and category based data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -18,14 +19,56 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req,res, next){
+  const authHeader = req.headers.authorization;
+  if(!authHeader){
+    return res.status(401).send('unauthorized access');
+  }
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+    if(err){
+      return res.status(403).send({message: 'forbidden access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+
+
+}
+
 async function run(){
   try{
     const phoneCategoriesCollection = client.db('phoneSpaceBD').collection('phoneCategories');
+    const allPhoneCategory = client.db('phoneSpaceBD').collection('categoryCollections');
     app.get('/phoneCategories', async(req,res)=>{
       const query = {};
       const options = await phoneCategoriesCollection.find(query).toArray();
       res.send(options);
     })
+
+    app.get('/category/:category_id',async (req,res)=>{
+      const category_id = req.params.category_id;
+      const query = {category_id: category_id};
+      const categoryPhones = await allPhoneCategory.find(query).toArray();
+      res.send(categoryPhones)
+    });
+
+    //jwt
+    app.get('/jwt',async(req, res)=>{
+      const email = req.query.email;
+      const query = {email: email};
+      const user = await usersCollection.findOne(query);
+      if(user){
+        const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '1d'})
+        return res.send({accessToken: token});
+
+      }
+
+      console.log(user);
+      res.status(403).send({accessToken: ''})
+    })
+
 
   }
   finally{
